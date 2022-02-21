46467ed2d73bf7fefcddc9b29461ab0766624866
Romes <thonyromes@gmail.com>
2022-02-21 22:37:37 +0100

    feat: Initialize project

---
+++ src/component/view/auth/login.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from 'react-native';
+
+import { TextView } from './styles';
+
+const login = () => {
+  return (
+    <View>
+      <TextView>login Screen</TextView>
+    </View>
+  );
+};
+
+export default login;
